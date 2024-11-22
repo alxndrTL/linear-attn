688a25625721b51ac2a8965a66ea4194cabf71a0
Alexandre TL <alextorresleguet@icloud.com>
2024-11-22 21:17:26 +0100

    refactor benchmark 2 + tr script

--- benchmark.py
+++ benchmark.py
@@ -11,51 +11,60 @@ from fla.ops.linear_attn.naive import naive_chunk_linear_attn as fla_chunkwise_a
 #TODO : backward
 
 device = "cpu"
-requires_grad = True
+requires_grad = False
 WARMUP_STEPS = 10
 MEASURE_STEPS = 10
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--type', type=str, required=True, help="chunkwise,recurent")
-parser.add_argument('--B', type=int, required=False, default=1)
-parser.add_argument('--L', type=int, required=False, default=1024)
-parser.add_argument('--D', type=int, required=False, default=128)
-parser.add_argument('--chunk_size', type=int, required=False, default=128)
-parser.add_argument('--profile', type=bool, required=False, default=False)
+def bench(B, L, D, chunk_size=None, profile=False):
+    # ensure same API for all functions
+    ATTN_FUNC = {'chunkwise': lambda Q,K,V: chunkwise_attn(Q, K, V, chunk_size=chunk_size),
+                 'recurrent': recurrent_attn,
+                 'fla_chunkwise': lambda Q,K,V: fla_chunkwise_attn(Q.unsqueeze(1), K.unsqueeze(1), V.unsqueeze(1), scale=1, chunk_size=chunk_size).squeeze(1)}
+    
+    Q = torch.randn(B, L, D, requires_grad=requires_grad).to(device=device)
+    K = torch.randn(B, L, D, requires_grad=requires_grad).to(device=device)
+    V = torch.randn(B, L, D, requires_grad=requires_grad).to(device=device)
 
-args = parser.parse_args()
+    func = ATTN_FUNC[args.type]
 
-# ensure same API for all functions
-ATTN_FUNC = {'chunkwise': lambda Q,K,V: chunkwise_attn(Q, K, V, chunk_size=args.chunk_size),
-             'recurrent': recurrent_attn,
-             'fla_chunkwise': lambda Q,K,V: fla_chunkwise_attn(Q.unsqueeze(1), K.unsqueeze(1), V.unsqueeze(1), scale=1, chunk_size=args.chunk_size).squeeze(1)}
-
-Q = torch.randn(args.B, args.L, args.D, requires_grad=requires_grad).to(device=device)
-K = torch.randn(args.B, args.L, args.D, requires_grad=requires_grad).to(device=device)
-V = torch.randn(args.B, args.L, args.D, requires_grad=requires_grad).to(device=device)
-
-func = ATTN_FUNC[args.type]
-
-for _ in range(WARMUP_STEPS):
-    func(Q, K, V)
-
-st = time.time_ns()
-for _ in range(MEASURE_STEPS):
-    func(Q, K, V)
-et = time.time_ns()
-
-t = (et - st) / MEASURE_STEPS / 1000000
-print(f'{t}ms')
-
-if args.profile:
-    with torch.profiler.profile(
-            activities=[torch.profiler.ProfilerActivity.CPU, torch.profiler.ProfilerActivity.CUDA],
-            on_trace_ready=torch.profiler.tensorboard_trace_handler('./profiler_logs/bench_log_flash'),
-            record_shapes=True,
-            profile_memory=True,
-            with_stack=False, # incurs an additional overhead, disable if not needed
-            with_flops=True,
-            with_modules=False, # only for torchscript models atm
-    ) as prof:
+    for _ in range(WARMUP_STEPS):
         func(Q, K, V)
-    print(prof.key_averages().table(sort_by="cuda_memory_usage", row_limit=10))
+
+    st = time.time_ns()
+    for _ in range(MEASURE_STEPS):
+        func(Q, K, V)
+    et = time.time_ns()
+
+    t = (et - st) / MEASURE_STEPS / 1000000
+
+    if args.profile:
+        with torch.profiler.profile(
+                activities=[torch.profiler.ProfilerActivity.CPU, torch.profiler.ProfilerActivity.CUDA],
+                on_trace_ready=torch.profiler.tensorboard_trace_handler('./profiler_logs/bench_log_flash'),
+                record_shapes=True,
+                profile_memory=True,
+                with_stack=False, # incurs an additional overhead, disable if not needed
+                with_flops=True,
+                with_modules=False, # only for torchscript models atm
+        ) as prof:
+            func(Q, K, V)
+        return t, prof.key_averages().table(sort_by="cuda_memory_usage", row_limit=10)
+
+    return t, None
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--type', type=str, required=True, help="chunkwise,recurent")
+    parser.add_argument('--B', type=int, required=False, default=1)
+    parser.add_argument('--L', type=int, required=False, default=1024)
+    parser.add_argument('--D', type=int, required=False, default=128)
+    parser.add_argument('--chunk_size', type=int, required=False, default=128)
+    parser.add_argument('--profile', type=bool, required=False, default=False)
+
+    args = parser.parse_args()
+
+    t, profile = bench(args.B, args.L, args.D, args.chunk_size, args.profile)
+    print(f'{t}ms')
+
+    if profile:
+        print(profile)
